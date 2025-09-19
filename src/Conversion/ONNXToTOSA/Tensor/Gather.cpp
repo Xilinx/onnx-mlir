b638d56cf963164c63cb1a54204aae698a871eac
planzase <pablo.lanzaserrano@amd.com>
2025-09-19 04:01:08 -0600

    check dynamic indices

--- src/Conversion/ONNXToTOSA/Tensor/Gather.cpp
+++ src/Conversion/ONNXToTOSA/Tensor/Gather.cpp
@@ -46,7 +46,8 @@ public:
     if (!onnx_mlir::isRankedShapedType(inputType))
       return rewriter.notifyMatchFailure(op, "input is not a ranked tensor");
 
-    if (!hasStaticShape(inputType) || !hasStaticShape(result.getType()))
+    if (!hasStaticShape(inputType) || !hasStaticShape(indices.getType()) ||
+        !hasStaticShape(result.getType()))
       return rewriter.notifyMatchFailure(op, "dynamic shapes not supported");
 
     auto resultTy = dyn_cast<TensorType>(op.getType());
