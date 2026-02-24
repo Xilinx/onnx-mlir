c1fd0acaeb587961d73d27b98ddf7dea9ea95e65
Ilango Rajagopal <ilangokul@gmail.com>
2026-02-24 03:07:44 -0700

    Update llvm-project commit & enable QTypes test

--- utils/clone-mlir.sh
+++ utils/clone-mlir.sh
@@ -1,3 +1,3 @@
 git clone -n https://github.com/xilinx/llvm-aie.git llvm-project
 # Check out a specific branch that is known to work with ONNX-MLIR.
-cd llvm-project && git checkout 60a1abaabb5721b7ee6f1a085072f9ad7e7bc430 && cd ..
+cd llvm-project && git checkout 0531ca15b5cadbb7e78d022a8ab637162db8e519 && cd ..
