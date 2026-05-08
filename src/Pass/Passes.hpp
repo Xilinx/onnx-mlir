75a871281126db8528aecbe049d0e666bc979946
Ilango Rajagopal <ilango.rajagopal@amd.com>
2026-05-08 07:11:00 -0600

    Enable const-folding for reformatting ops with QDQ

--- src/Pass/Passes.hpp
+++ src/Pass/Passes.hpp
@@ -57,7 +57,8 @@ std::unique_ptr<mlir::Pass> createShapeInferencePass();
 void configureConstPropONNXToONNXPass(bool roundFPToInt, int expansionBound,
     llvm::ArrayRef<std::string> disabledPatterns, bool constantPropIsDisabled);
 
-std::unique_ptr<mlir::Pass> createConstPropONNXToONNXPass();
+std::unique_ptr<mlir::Pass> createConstPropONNXToONNXPass(
+    bool enableQDQ = false);
 
 std::unique_ptr<mlir::Pass> createQDQCanonicalizePass(
     bool removeBinary = false, bool removeQDQAroundOps = false);
