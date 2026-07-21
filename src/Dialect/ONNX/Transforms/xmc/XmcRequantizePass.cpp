8fd32d0dd65855e9de48fe390640df362f9c339a
Ayush Anshul <ayanshul@amd.com>
2026-07-20 21:58:44 -0600

    resultname should be on requant of df ops

--- src/Dialect/ONNX/Transforms/xmc/XmcRequantizePass.cpp
+++ src/Dialect/ONNX/Transforms/xmc/XmcRequantizePass.cpp
@@ -75,15 +75,20 @@ struct InputDictatesRequantizePattern : public OpRewritePattern<OpType> {
     auto resultTy = cast<RankedTensorType>(result.getType());
     auto retypedTy = RankedTensorType::get(resultTy.getShape(), inQ);
 
-    rewriter.modifyOpInPlace(op, [&]() { result.setType(retypedTy); });
+    // Recreate the data-flow op producing the input quant type. Drop the
+    // inherited ResultNames so the greedy driver's ResultNamesUpdater listener
+    // can derive a fresh intermediate name for it; the boundary name migrates
+    // onto the Requantize via the replaceOp below.
+    auto newDataFlow = rewriter.create<OpType>(
+        op.getLoc(), TypeRange{retypedTy}, op->getOperands(), op->getAttrs());
+    newDataFlow->removeAttr("ResultNames");
 
-    rewriter.setInsertionPointAfter(op);
-    auto requant =
-        rewriter.create<XCOMPILERRequantizeOp>(op.getLoc(), resultTy, result,
-            buildScaleAttr(rewriter, inQ), buildZeroPointAttr(rewriter, inQ),
-            buildScaleAttr(rewriter, outQ), buildZeroPointAttr(rewriter, outQ));
+    auto requant = rewriter.create<XCOMPILERRequantizeOp>(op.getLoc(), resultTy,
+        newDataFlow->getResult(0), buildScaleAttr(rewriter, inQ),
+        buildZeroPointAttr(rewriter, inQ), buildScaleAttr(rewriter, outQ),
+        buildZeroPointAttr(rewriter, outQ));
 
-    rewriter.replaceAllUsesExcept(result, requant.getResult(), requant);
+    rewriter.replaceOp(op, requant.getResult());
     return success();
   }
 };
